ee064c2fc8b30665b969bc713f366a407891bf2d
dhilipakaran <dhilip14fire@gmail.com>
2025-04-26 19:25:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,18 @@
 provider "google" {
-  project = ""
-  region  = ""
+  project = "galvanic-portal-456405-a2"
+  region  = "us-central1"
 }
-resource "google_cloud_run_service" "my_app" {
-  name     = "my_application"
-  region   = ""
+
+resource "google_cloud_run_service" "my-app" {
+  name     = "my-app"
+  location = "us-central1"
 
   template {
     spec {
       containers {
-        image = ""
+        image = "docker.io/dhilipakaran/testing:v1"
         ports {
-          container_port =
+          container_port = 8080
         }
       }
     }
@@ -25,9 +26,13 @@ resource "google_cloud_run_service" "my_app" {
   autogenerate_revision_name = true
 }
 
-resource "google_cloud_run_service_iam_member" "invoker" {
-  region   = google_cloud_run_service.my_app.location
-  service  = google_cloud_run_service.my_app.name
+resource "google_cloud_run_service_iam_member" "public_access" {
+  location = google_cloud_run_service.my-app.location
+  service  = google_cloud_run_service.my-app.name
   role     = "roles/run.invoker"
   member   = "allUsers"
+}
+
+output "cloud_run_url" {
+  value = google_cloud_run_service.my-app.status[0].url
 }
